9661ffbbe3092a901e2220ae4152fef190817fb6
Yuval Gat <yuvalgat11@gmail.com>
2020-12-08 17:36:04 +0200

    Fix double space check

--- double_space.py
+++ double_space.py
@@ -2,6 +2,11 @@ import re
 
 
 def correct_double_space(text):
+    """
+
+    :param text: text to be corrected
+    :return: text where double spaces are replaced with a single space
+    """
     double_space_regex = r'  '
     text = re.sub(double_space_regex, ' ', text)
     return text
