f4dda8665f7a79f0348f1a8575cb4c0fc394c781
Yuval Gat <yuvalgat11@gmail.com>
2020-12-08 18:52:57 +0200

    Show differences in website

--- corrections/punctuation.py
+++ corrections/punctuation.py
@@ -13,4 +13,6 @@ def correct_punctuation(text):
     text = re.sub(digit_punctuation_regex, r'\1\2\3', text)
     opening_parentheses_regex = r'([א-ת0-9])\('  # Ensure space before opening parentheses
     text = re.sub(opening_parentheses_regex, r'\1 (', text)
+    question_mark_space_regex = r' \?'  # Ensure no space before question mark
+    text = re.sub(question_mark_space_regex, '?', text)
     return text
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+
 from corrections.dashes import *
 from corrections.punctuation import *
 from corrections.double_space import *
+
+import difflib
+import webbrowser
+import pathlib
 
 # All correction methods
 corrections = [correct_dashes, correct_punctuation, correct_double_space]
@@ -14,5 +20,17 @@ with open('./text.txt', 'r+') as file:
 
     with open('./corrected_text.txt', 'w') as corrected_file:
         corrected_file.write(corrected_text)
+
+    d = difflib.HtmlDiff()
+    diff = d.make_file(text.split('\n'), corrected_text.split('\n'))
+    diff = diff.replace('<body>', '<body dir=\'rtl\'>')
+    diff = diff.replace('(f)irst change', '<u>f</u>irst change')
+    diff = diff.replace('(n)ext change', '<u>n</u>ext change')
+    diff = diff.replace('(t)op', '<u>t</u>op')
+
+    with open('./recommendations.html', 'w', encoding='utf-8') as recommendations_file:
+        recommendations_file.write(diff)
+
+    os.system('open recommendations.html')
 
     print(corrected_text)
